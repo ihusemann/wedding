2a614f4deccdc9edb2752709bb211a3ec174470b
Isaac Husemann <isaac.husemann@gmail.com>
2025-01-05 13:51:19 -0600

    trpc init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,13 @@
+import { trpc } from "@/trpc/server";
 import { Button } from "@repo/ui/components/button";
 
-export default function Page() {
+export default async function Page() {
+  const data = await trpc.hello({ text: "isaac" });
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">{data.greeting}</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
