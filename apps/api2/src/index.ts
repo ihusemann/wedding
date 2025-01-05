87f9991f865362bd7780e22d3e858c1e7851c6eb
Isaac Husemann <isaac.husemann@gmail.com>
2025-01-05 09:46:03 -0600

    openapi with hono

--- apps/api2/src/index.ts
+++ apps/api2/src/index.ts
@@ -1,12 +1,39 @@
 import { serve } from "@hono/node-server";
-import { Hono } from "hono";
 import { trialSchema } from "@repo/schemas/trial";
 import { zValidator } from "@hono/zod-validator";
 import { PrismaClient } from "@repo/db";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import { listTrialsSchema } from "@repo/schemas/trial";
+import { apiReference } from "@scalar/hono-api-reference";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
 const db = new PrismaClient();
+
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    operationId: "listTrials",
+    summary: "List trials",
+    tags: ["Trials"],
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: listTrialsSchema,
+          },
+        },
+        description: "Lists trials",
+      },
+    },
+  }),
+  async (c) => {
+    const trials = await db.trial.findMany();
+
+    return c.json(trials);
+  }
+);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -21,6 +48,25 @@ app.post("/", zValidator("json", trialSchema), async (c) => {
 
   return c.json(result);
 });
+
+// The OpenAPI documentation will be available at /doc
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "My API",
+    description: "Lorem ipsum",
+  },
+});
+
+app.get(
+  "/api",
+  apiReference({
+    spec: {
+      url: "/doc",
+    },
+  })
+);
 
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
--- packages/schemas/src/trial.ts
+++ packages/schemas/src/trial.ts
@@ -3,3 +3,5 @@ import { z } from "zod";
 export const trialSchema = z.object({
   name: z.string(),
 });
+
+export const listTrialsSchema = z.array(trialSchema);
